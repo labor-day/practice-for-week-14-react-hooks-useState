f188f27f22982edbb25e7b2d0f6770446705710e
Scott Shaffer <scottwshaffer1@gmail.com>
2022-09-21 19:43:48 -0600

    practiced useState hook

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import "./UseState.css";
+
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+  const themeChanger = () => {
+    let newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
   return (
-    <div>
+    <div className={`state ${theme}`}>
       <h1>UseState Component</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={themeChanger}>Dark</button>
     </div>
   );
 };
